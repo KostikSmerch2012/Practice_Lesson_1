3c15e16e244a2d83d0baaf10a6a57b8e2b09e2db
KostikSmerch2012 <danyatricov@gmail.com>
2025-09-16 14:09:23 +0300

    upgraded task 3

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,7 +9,7 @@
 
 const double PI = 3.14159265358979323846;
 
-void enter_func(int* a, int* b, int* c) {
+void enter_func(double* a, double* b, double* c) {
     std::cout << "First number: ";
     std::cin >> *a;
     std::cout << "Second number: ";
@@ -24,48 +24,51 @@ void task1_1() {
 }
 
 void task1_2() {
-    int a, b, c;
+    double a, b, c;
     enter_func(&a, &b, &c);
-    std::cout << a + b << "<- Summary" << std::endl;
-    std::cout << a - b << "<- Deduction" << std::endl;
-    std::cout << a * b << "<- Multiplying" << std::endl;
-    if (a != 0 || b != 0 and c != 0) {
-        std::cout << a / b << "<- Division" << std::endl;
+    std::cout << a + b << "<- Сумма" << std::endl;
+    std::cout << a - b << "<- Разность" << std::endl;
+    std::cout << a * b << "<- Произведение" << std::endl;
+    if (a != 0 || b != 0) {
+        std::cout << a / b << "<- Делелие" << std::endl;
     }
     else {
-        std::cout << "Cant be zero!";
+        std::cout << "Знаменатель не может быть нулём!";
     }
 }
 
 void task1_3() {
-    double a, b;
+    double b, c;
     std::cout << "First number: \n";
-    std::cin >> a;
-    std::cout << "Second number: \n";
     std::cin >> b;
-    if (a != 0) {
-        std::cout << -b / a << std::endl;
+    std::cout << "Second number: \n";
+    std::cin >> c;
+    if (b != 0) {
+        std::cout << -c / b << std::endl;
     }
-    else {
-        std::cout << "0" << std::endl;
+    if (b == 0 && c == 0) {
+        std::cout << "x - любой" << std::endl;
+    }
+    if (b == 0 && c != 0) {
+        std::cout << "Нет решений" << std::endl;
     }
 }
 
 void task1_4() {
-    double a, b, c, m;
+    double a, b, c, D;
     std::cout << "First number: ";
     std::cin >> a;
     std::cout << "Second number: ";
     std::cin >> b;
     std::cout << "Second number: ";
     std::cin >> c;
-    m = pow(b, 2) - 4 * a * c;
-    if (m < 0) {
+    D = pow(b, 2) - 4 * a * c;
+    if (D < 0) {
         std::cout << "Нет корней" << std::endl;
     }
     if (a != 0 || b != 0 || c != 0) {
-        std::cout << (-b + pow(m, 0.5)) / (2 * a) << std::endl;
-        std::cout << (-b - pow(m, 0.5)) / (2 * a) << std::endl;
+        std::cout << (-b + pow(D, 0.5)) / (2 * a) << std::endl;
+        std::cout << (-b - pow(D, 0.5)) / (2 * a) << std::endl;
     }
     if (b == 0 && c == 0) {
         std::cout << "х - Любой" << std::endl;
@@ -91,7 +94,7 @@ void task1_5() {
 
 
 void task1_6() {
-    double R, r, h, V;
+    double R, r, h;
     std::cout << "Радиус большего основания: " << std::endl;
     std::cin >> R;
     std::cout << "Радиус меньшего основания: " << std::endl;
@@ -113,6 +116,6 @@ void task1_6() {
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    task1_5();
+    task1_3();
     return 0;
 }
